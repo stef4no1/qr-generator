0a0b66d50a2d47212270202845dff2d88e7f9521
Stefano Straga <stefanostraga2k18@gmail.com>
2022-09-12 19:24:16 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import qrcode
+import image
+qr = qrcode.QRCode(version=15, box_size=10,border=5)
+
+#Aca pones el link de donde queres que redigira el QR
+data = "https://github.com/stef4no1"
+
+qr.add_data(data)
+qr.make(fit=True)
+img = qr.make_image(fill="black", back_color="white")
+img.save("test.png")
+
+
